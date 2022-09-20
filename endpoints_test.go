74be0f332785c081272d6b88d2c84c70c6d9800a
0num4 <r.oonuma@matsuri-tech.com>
2022-09-20 14:06:11 +0900

    feat: とりあえず修正できそうなのでテストは適当に書き上げる

--- endpoints_test.go
+++ endpoints_test.go
@@ -1,11 +1,12 @@
 package endpoints
 
 import (
+	"reflect"
+	"testing"
+
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/iancoleman/orderedmap"
 	"github.com/labstack/gommon/log"
-	"reflect"
-	"testing"
 )
 
 type fields struct {
@@ -18,48 +19,67 @@ type args struct {
 	frontend string
 }
 
+// path /owners/ownerHistory/:id/
+// flag :id
+// name id
+// replace後path /owners/ownerHistory/{id}/
+
 // m2m-coreでデバッグした実値
-var m2mCoreAPIListRealValue = []API{
-	{
-		Name:   "GetMatsuriListingOwner",
-		Path:   "/api/v1/matsuri_listing_owner/{id}",
-		Desc:   "マツリリストオーナーを取得する",
-		Method: "GET",
-		Versions: Versions{
-			"v1",
+var m2mCoreRealValueFieldsStruct = fields{
+	env: []Env{
+		{
+			Version: "v1",
+			Domain: Domain{
+				Local:    "http://localhost:8000",
+				LocalDev: "https://api-core.dev.m2msystems.cloud",
+				Dev:      "https://api-core.dev.m2msystems.cloud",
+				Prod:     "https://api-core.m2msystems.cloud",
+			},
 		},
 	},
-	{
-		Name:   "healthCheck",
-		Path:   "/api/v1/health_check",
-		Desc:   "ヘルスチェック",
-		Method: "GET",
-	},
-	{
-		Name:   "updateOwnerHistory",
-		Path:   "owners/ownerHistory/:id/",
-		Desc:   "ownerHistoryIdを使って該当のオーナー履歴を更新する",
-		Method: "PATCH",
-	},
-	{
-		Name:   "deleteOwnerHistory",
-		Path:   "owners/ownerHistory/:id/",
-		Desc:   "指定idのオーナー履歴を削除する",
-		Method: "DELETE",
-	},
-	{
-		Name: "findById",
-		Path: "owners/ownerHistory/:id/",
-		// ↓なぜか文字化けしてた
-		//Desc:   "ownerHistoryIdを使って管理しているリスティングのオーナー履歴一覧を取得する",
-		Desc:   "ownerHistoryIdを使って管理しているlist一覧を取得",
-		Method: "GET",
-	},
-	{
-		Name:   "findAllByListingId",
-		Path:   "owners/listings/:listingId/ownerHistories?listingId=xxx",
-		Desc:   "listingIdを使って管理しているリスティングのオーナー履歴一覧を取得する",
-		Method: "GET",
+	frontends: []string{"web", "admin"},
+	api: []API{
+		{
+			Name:   "GetMatsuriListingOwner",
+			Path:   "/api/v1/matsuri_listing_owner/{id}",
+			Desc:   "マツリリストオーナーを取得する",
+			Method: "GET",
+			Versions: Versions{
+				"v1",
+			},
+		},
+		{
+			Name:   "healthCheck",
+			Path:   "/api/v1/health_check",
+			Desc:   "ヘルスチェック",
+			Method: "GET",
+		},
+		{
+			Name:   "updateOwnerHistory",
+			Path:   "owners/ownerHistory/:id/",
+			Desc:   "ownerHistoryIdを使って該当のオーナー履歴を更新する",
+			Method: "PATCH",
+		},
+		{
+			Name:   "deleteOwnerHistory",
+			Path:   "owners/ownerHistory/:id/",
+			Desc:   "指定idのオーナー履歴を削除する",
+			Method: "DELETE",
+		},
+		{
+			Name: "findById",
+			Path: "owners/ownerHistory/:id/",
+			// ↓なぜか文字化けしてた
+			//Desc:   "ownerHistoryIdを使って管理しているリスティングのオーナー履歴一覧を取得する",
+			Desc:   "ownerHistoryIdを使って管理しているlist一覧を取得",
+			Method: "GET",
+		},
+		{
+			Name:   "findAllByListingId",
+			Path:   "owners/listings/:listingId/ownerHistories?listingId=xxx",
+			Desc:   "listingIdを使って管理しているリスティングのオーナー履歴一覧を取得する",
+			Method: "GET",
+		},
 	},
 }
 var normalTestCaseFieldStruct = fields{
@@ -225,7 +245,8 @@ func Test_endpoints_generateOpenApiSchema(t *testing.T) {
 			fields: normalTestCaseFieldStruct,
 		},
 		{
-			name: "m2m-coreでデバッグした実値",
+			name:   "m2m-coreでデバッグした実値",
+			fields: m2mCoreRealValueFieldsStruct,
 		},
 	}
 	for _, tt := range tests {
